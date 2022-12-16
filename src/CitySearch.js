52e4cf48dca47120b4f977336ff0e818557f276f
Kathanira <112494140+Kathanira@users.noreply.github.com>
2022-12-16 02:26:57 +0100

    Added InfoAlert & ErrorAlert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
@@ -8,23 +9,38 @@ class CitySearch extends Component {
   };
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({showSuggestions:true});
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({ query: value, suggestions });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText:''
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
      this.setState({
-       query: suggestion,
-       showSuggestions: false,
+      query: suggestion,
+      suggestions: [],
+      showSuggestions:false,
+      infoText:''
      });
      this.props.updateEvents(suggestion);
    };
 
    render() {
     return (
-      <div className="CitySearch">
+      <div className="CitySearch" onBlur={this.hideSuggestions}>
+         <InfoAlert text={this.state.infoText} />
        <input
          type="text"
           className="city"
